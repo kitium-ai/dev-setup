02488ef489baea6ea24bf0a4edcf18d66f0a75ac
ASHISH YADAV <ashish.yd@gmail.com>
2025-11-29 20:24:56 +0000

    fix: vitest config changed

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,18 @@
+import baseConfig from '@kitiumai/config/vitest.config.base.js';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  ...baseConfig,
   test: {
+    ...baseConfig.test,
     globals: true,
     environment: 'node',
     include: ['src/**/*.test.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      'src/__tests__/utils.test.ts',
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
